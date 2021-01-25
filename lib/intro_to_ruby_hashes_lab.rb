c9982665d7dc4cf17170c714e8a0d121bc197c49
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-25 23:54:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  my_hash = {name: key, species: value}
+  my_hash = {key: value}
   my_hash
 end
 
