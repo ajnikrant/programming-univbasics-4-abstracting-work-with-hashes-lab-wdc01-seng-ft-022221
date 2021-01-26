505ad5354adfe390649f9d463c20e9bb44caf0d3
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-26 00:00:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -6,6 +6,8 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  new_hash = {name: "Paprika", species: "cat"}
+  puts new_hash{:key}
 end
 
 def update_counting_hash(hash, key)
